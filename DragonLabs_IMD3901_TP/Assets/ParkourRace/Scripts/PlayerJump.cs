b968b7cb9263f27b5839bca0f6f3915d8f334f89
Rachel <rachelf2278@gmail.com>
2026-02-11 23:31:12 -0500

    Jump script
    When the player presses space the player jumps

--- DragonLabs_IMD3901_TP/Assets/ParkourRace/Scripts/PlayerJump.cs
+++ DragonLabs_IMD3901_TP/Assets/ParkourRace/Scripts/PlayerJump.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerJump : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float jumpHeight = 1.2f;
+    public float gravity = -20f;
+    public float groundCheckDistance = 0.2f;
+
+    private CharacterController controller;
+    private float verticalVelocity;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        verticalVelocity += gravity * Time.deltaTime;
+        controller.Move(Vector3.up * verticalVelocity * Time.deltaTime);
     }
 }
