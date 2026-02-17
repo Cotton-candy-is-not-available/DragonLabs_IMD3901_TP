56a420bb85f28308e43daf46b24c081e36e8836f
bbwebz <bitanazar5984@gmail.com>
2026-02-17 16:40:02 -0500

    game timer
    added game timer and pinata health that decreases with hits

--- DragonLabs_IMD3901_TP/Assets/PiñataPop/Scripts/PiñataController.cs
+++ DragonLabs_IMD3901_TP/Assets/PiñataPop/Scripts/PiñataController.cs
@@ -3,18 +3,21 @@ using UnityEngine;
 
 public class PiñataController : MonoBehaviour
 {
-    public ScoresManager scoresManager_access;
 
-    void Start()
-    {
-        
-    }
+    int piñataHealth = 50;
+    public bool isGameOver = false;
+    public ScoresManager scoresManager_access;
 
     void Update()
     {
-        
-    }
+        Debug.Log("pinata health: " + piñataHealth);
 
+        if (piñataHealth == 0)
+        {
+            Debug.Log("GAME OVER!");
+            isGameOver = true;
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         
@@ -22,13 +25,19 @@ public class PiñataController : MonoBehaviour
         {
             Debug.Log("P1 hit the piñata");
             scoresManager_access.increaseP1Hits();
+            if(piñataHealth > 0 && isGameOver == false)
+            {
+                piñataHealth -= 1;
+            }
         }
         else if(collision.gameObject.name == "BatP2")
         {
             Debug.Log("P2 hit the piñata");
             scoresManager_access.increaseP2Hits();
+            if (piñataHealth > 0 && isGameOver == false)
+            {
+                piñataHealth -= 1;
+            }
         }
-
-
     }
 }
---
+++ DragonLabs_IMD3901_TP/Assets/PiñataPop/Scripts/TimerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField] TMPro.TextMeshProUGUI timerDisplay;
+    float elapsedTime = 90.0f;
+    public PiñataController piñataController_access;
+
+    void Update()
+    {
+        if (piñataController_access.isGameOver == false) //if the game is not over the timer should be counting
+        {
+            elapsedTime -= Time.deltaTime; //calculates all of the time passed since game started
+            int minutes = Mathf.FloorToInt(elapsedTime / 60);
+            int seconds = Mathf.FloorToInt(elapsedTime % 60);
+
+            //formats the minutes and seconds to display as 00:00
+            timerDisplay.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else //if the game is over (either failure or victory), reset the timer
+        {
+            elapsedTime = 0;
+            timerDisplay.text = "00:00";
+        }
+    }
+}
