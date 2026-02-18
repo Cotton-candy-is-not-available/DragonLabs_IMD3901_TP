c935ddf03e5515013488b0c412901471da25d795
bbwebz <bitanazar5984@gmail.com>
2026-02-17 19:18:32 -0500

    added hit charge circle
    charge hit attack circle now works with left mouse click on PC

--- DragonLabs_IMD3901_TP/Assets/PiñataPop/Scripts/BatChargeHits.cs
+++ DragonLabs_IMD3901_TP/Assets/PiñataPop/Scripts/BatChargeHits.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class BatChargeHits : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float indicatorTimer = 1.0f;
+    [SerializeField] private float maxIndicatorTimer = 1.0f;
+    [SerializeField] private Image hitChargeCircle;
+    [SerializeField] private KeyCode selectKey = KeyCode.Mouse0; //left mouse click for PC
+    [SerializeField] private bool shouldUpdate = false;
+    [SerializeField] private UnityEvent pinataForce;
+   
+    private void Update()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Input.GetKey(selectKey)) //if LMB is being held down
+        {
+            indicatorTimer -= Time.deltaTime;
+            hitChargeCircle.enabled = true;
+            hitChargeCircle.fillAmount = indicatorTimer;
+
+            //for looping its charge again
+            if (indicatorTimer <= 0)
+            {
+                shouldUpdate = false;
+                indicatorTimer = maxIndicatorTimer;
+                hitChargeCircle.fillAmount = maxIndicatorTimer;
+                hitChargeCircle.enabled = false;
+                pinataForce.Invoke();
+            }
+        }
+        else //if the LMB is NOT being held down anymore
+        {
+            if(shouldUpdate == true)
+            {
+                indicatorTimer += Time.deltaTime;
+                hitChargeCircle.fillAmount = indicatorTimer;
+
+                if(indicatorTimer >= maxIndicatorTimer)
+                {
+                    indicatorTimer = maxIndicatorTimer;
+                    hitChargeCircle.fillAmount = maxIndicatorTimer;
+                    hitChargeCircle.enabled = false;
+                    shouldUpdate = false;
+                }
+            }
+        }
+
+        if (Input.GetKey(selectKey))
+        {
+            shouldUpdate = true;
+        }
     }
 }
