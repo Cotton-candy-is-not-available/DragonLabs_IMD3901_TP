174a1c86e3ec93820f138452c8f43430f72d40c2
Rachel <rachelf2278@gmail.com>
2026-02-19 21:07:59 -0500

    added timer
    timer runs from the start (will change so it starts after a countdown). when button is pressed timer stops

--- DragonLabs_IMD3901_TP/Assets/ParkourRace/Scripts/Button.cs
+++ DragonLabs_IMD3901_TP/Assets/ParkourRace/Scripts/Button.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class Button : MonoBehaviour
 {
+    public float interactRange = 2f;
+    public Camera playerCam;
+
     public float range = 0.5f;
     private float speed = 0.5f;
     private Vector3 startPos;
 
     private bool isPressed = false;
     private bool isAnimating = false;
+
+    private float timer = 0f;
+    private bool isTimerRunning = true;
+
+    public TMP_Text timerText;
 
     void Start()
     {
@@ -17,9 +27,26 @@ public class Button : MonoBehaviour
 
     void Update()
     {
-        if (Keyboard.current.iKey.wasPressedThisFrame)
+        if (isTimerRunning)
         {
-            Press();
+            timer += Time.deltaTime;
+            int mins = Mathf.FloorToInt(timer / 60f);
+            int seconds = Mathf.FloorToInt(timer % 60f);
+            int milliseconds = Mathf.FloorToInt((timer * 1000f) % 1000f);
+            timerText.text = string.Format("{0:00}:{1:00}:{2:000}", mins, seconds, milliseconds);
+        }
+
+        Ray ray = new Ray(playerCam.transform.position, playerCam.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, interactRange))
+        {
+            if (hit.collider.CompareTag("Interactable"))
+            {
+                if (Keyboard.current.iKey.wasPressedThisFrame)
+                {
+                    Press();
+                }
+            }
         }
 
         Vector3 target = isPressed ? startPos - Vector3.up * range : startPos;
@@ -38,6 +65,7 @@ public class Button : MonoBehaviour
     {
         isPressed = true;
         isAnimating = true;
+        isTimerRunning = false;
         Debug.Log("Pressed");
     }
 }
