122f743579a8d9eb7ca61c3d6bfbffd2a0010e35
Rachel <rachelf2278@gmail.com>
2026-03-03 18:45:13 -0500

    Created spawn point
    If the player falls, when they touch the floor, they will be teleported to the start

---
+++ DragonLabs_IMD3901_TP/Assets/ParkourRace/Scripts/SpawnPoint.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CharacterController controller = other.GetComponent<CharacterController>();
+
+            if (controller != null)
+            {
+                controller.enabled = false;
+                other.transform.position = spawnPoint.position;
+                controller.enabled = true;
+            }
+            else
+            {
+                other.transform.position = spawnPoint.position;
+            }
+        }
+    }
+}
