c27d16864d7bd05140a6d3c9b3cf9a722c2709b9
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2026-03-07 04:04:11 -0500

    added online canvas and buttons to lobby scene

--- DragonLabs_IMD3901_TP/Assets/GeneralScripts/onlineNetcodeScripts/startRelay.cs
+++ DragonLabs_IMD3901_TP/Assets/GeneralScripts/onlineNetcodeScripts/startRelay.cs
@@ -27,24 +27,6 @@ public class startRelay : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI joinCodeDisplay;
 
-    //public async Task<string> StartHostWithRelay(int maxConnections, string connectionType)
-    //{
-    //    await UnityServices.InitializeAsync();//connect to unity services
-    //    if (!AuthenticationService.Instance.IsSignedIn)
-    //    {
-    //        Debug.Log("Signed in: " + AuthenticationService.Instance.PlayerId);//get player ID
-    //        await AuthenticationService.Instance.SignInAnonymouslyAsync();//creates an account for the user anonymously
-    //    }
-    //    var allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
-    //    NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, "connectionType"));
-    //
-    //
-    //
-    //    var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-    //    return NetworkManager.Singleton.StartHost() ? joinCode : null;
-    //}
-
-    //These functions get called when the host or client start
     private void Awake()
     {
         hostButton.onClick.AddListener(() =>
@@ -63,26 +45,11 @@ public class startRelay : MonoBehaviour
         await UnityServices.InitializeAsync();//initialises unity services so that API and relay can run
         AuthenticationService.Instance.SignedIn += () =>
         {
-            Debug.Log("Signed in: " + AuthenticationService.Instance.PlayerId);
+            Debug.Log("Signed in: " + AuthenticationService.Instance.PlayerId);//get player ID
         };
         await AuthenticationService.Instance.SignInAnonymouslyAsync();//creates account for user anonymously
 
     }
-
-
-    //public async void createRelay()
-    //{
-    //        try
-    //        {
-    //            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(2);//max number of players
-    //            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-    //        }
-    //        catch (RelayServiceException ex)
-    //        {
-    //            Debug.Log("RelayServiceException: " + ex);
-    //        }
-        
-    //}
 
     //Same as create host button
     private async void createRelay()//creates an instance of relay so that user can connect to online service
@@ -96,11 +63,8 @@ public class startRelay : MonoBehaviour
             Debug.Log(joinCode);
 
             joinCodeDisplay.text = joinCode; //display join code on the screen
-
-            //RelayServerData relayServerData = new RelayServerData(allocation, "wss");
+            
             NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, "wss"));//web socket connection type
-
-            //NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
 
             NetworkManager.Singleton.StartHost();
             joinPanel.SetActive(false);//hide the join panel
@@ -119,11 +83,7 @@ public class startRelay : MonoBehaviour
             Debug.Log("Joining relay with: " + joinCode);
             JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
 
-            // RelayServerData relayServerData = new RelayServerData(joinAllocation, "wss");
-
             NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(joinAllocation, "wss"));
-
-            //NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
 
             NetworkManager.Singleton.StartClient();
             joinPanel.SetActive(false);//hide the join panel
