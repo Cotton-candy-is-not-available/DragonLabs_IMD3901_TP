cb559100bae278cf346d0c6f225fab1eb65dc7c1
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2026-03-12 00:16:28 -0400

    working offline single player switch scenes button

---
+++ DragonLabs_IMD3901_TP/Assets/GeneralScripts/sceneManagement/ChooseGame.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChooseGame : MonoBehaviour
+{
+    //public Animator transition;
+
+    public void switchScenes(string sceneName)
+    {
+        //transition.SetTrigger("Start");
+        SceneManager.LoadScene(sceneName);
+        StartCoroutine(delaySec(sceneName));
+        Debug.Log("before calling coroutine");
+    }
+
+    IEnumerator delaySec(string sceneName)
+    {
+        Time.timeScale = 1f;
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("after calling coroutine");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void switchScenesOverlay(string sceneName2)
+    {
+        SceneManager.LoadScene(sceneName2, LoadSceneMode.Additive);
+    }
+
+    public void OnExitClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
