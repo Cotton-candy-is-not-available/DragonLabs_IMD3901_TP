f76c8a076a97d3049d981fa99f8df635fd5382a4
bbwebz <bitanazar5984@gmail.com>
2026-03-15 19:28:05 -0400

    added audio manager prefab
    will work with the audio's of other minigames, just the code needs to be un-commented
    
    currently the sound does not synch between host and client

---
+++ DragonLabs_IMD3901_TP/Assets/PiñataPop/Scripts/AudioManager.cs
@@ -0,0 +1,70 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AudioManager : NetworkBehaviour
+{
+    [Header("---- Audio Source ----")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("---- Audio Clip ----")]
+    public AudioClip pinata_background;
+    /* add other background music for the other minigames
+    public AudioClip beerPong_background;
+    public AudioClip ticTacToe_background;
+    public AudioClip parkour_background; */
+
+    public string chosenGame = null;
+
+
+    private void Start()
+    {
+        //check which minigame we are in, and play the corresponding background music
+        PlayBackgroundMusic(chosenGame);
+    }
+
+
+    //[ServerRpc(RequireOwnership = false)]
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+
+
+    //takes in a string of which game is selected (aka which minigame's scene the audio manager prfab is in)
+    //[ServerRpc(RequireOwnership = false)]
+    public void PlayBackgroundMusic(string chosenGame) 
+    {
+        //SFXSource.PlayOneShot(clip);
+        switch (chosenGame)
+        {
+            case "PinataPop":
+                musicSource.clip = pinata_background;
+                musicSource.Play();
+                break;
+
+            /*
+            case "BeerPong":
+                musicSource.clip = beerPong_background;
+                musicSource.Play();
+                break;
+
+            case "TicTacToe":
+                musicSource.clip = ticTacToe_background;
+                musicSource.Play();
+                break;
+
+            case "ParkourRace":
+                musicSource.clip = parkour_background;
+                musicSource.Play();
+                break;
+            */
+        }
+    }
+
+
+
+
+
+}
