7337ce0f4d61c2e240d632ea94adaa2ca3a8349d
MKoffi <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2026-03-20 12:15:22 -0400

    start beer pour

--- DragonLabs_IMD3901_TP/Assets/BeerPong/beerPongScripts/beerScripts/startBlurEffect.cs
+++ DragonLabs_IMD3901_TP/Assets/BeerPong/beerPongScripts/beerScripts/startBlurEffect.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class startBlurEffect : MonoBehaviour
 {
+    public GameObject beer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
