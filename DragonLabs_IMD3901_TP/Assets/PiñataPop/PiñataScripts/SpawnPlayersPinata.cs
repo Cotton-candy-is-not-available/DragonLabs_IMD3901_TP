f3aa65635dba533aae7976aeab35e5184ba8ea76
bbwebz <bitanazar5984@gmail.com>
2026-03-31 13:58:47 -0400

    code cleanup

--- DragonLabs_IMD3901_TP/Assets/PiñataPop/PiñataScripts/SpawnPlayersPinata.cs
+++ DragonLabs_IMD3901_TP/Assets/PiñataPop/PiñataScripts/SpawnPlayersPinata.cs
@@ -6,9 +6,7 @@ using UnityEngine;
 
 public class SpawnPlayersPinata : NetworkBehaviour
 {
-    // public NetworkObject player1;
-    //public NetworkObject player2;
-
+    
     public GameObject player1;
     public GameObject player2;
 
@@ -27,55 +25,5 @@ public class SpawnPlayersPinata : NetworkBehaviour
         Debug.Log("client joined the scene");
         player2.transform.transform.position = p2StartPos.position;
     }
-
-    /*
-        public override void OnNetworkSpawn()
-        {
-            if (!IsServer)
-            {
-                return;
-            }
-
-            //find both player in the scene
-            //player1 = GameObject.FindWithTag("Player1");
-            //player2 = GameObject.FindWithTag("Player2");
-
-
-
-
-
-            *//*
-            //spawning the players at their given spawn points
-            if (NetworkManager.Singleton.LocalClientId == 0) //host
-            {
-                Debug.Log("placed host at spawn point");
-                //player1.transform.transform.position = p1StartPos.position;
-                player1.transform.transform.position = p1StartPos.transform.position;
-
-            }
-            else if (NetworkManager.Singleton.LocalClientId == 1) //client
-            {
-                Debug.Log("placed client at spawn point");
-                //player2.transform.transform.position = p2StartPos.position;
-                player2.transform.transform.position = p2StartPos.transform.position;
-            }*/
-
-    /*
-        private void spawnP1()
-        {
-            Debug.Log("placed host at spawn point");
-            //player1.transform.transform.position = p1StartPos.position;
-            player1.transform.transform.position = p1StartPos.transform.position;
-
-        }
-
-        private void spawnP2()
-        {
-            Debug.Log("placed client at spawn point");
-            //player2.transform.transform.position = p2StartPos.position;
-            player2.transform.transform.position = p2StartPos.transform.position;
-        }
-    *//*
-
-}*/
+    
 }
