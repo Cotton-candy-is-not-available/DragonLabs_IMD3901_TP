aefea50d74d7c4a71812b6e7370a41b0c9590716
Cotton-candy-is-not-available <156360240+Cotton-candy-is-not-available@users.noreply.github.com>
2026-04-03 17:20:37 -0400

    scaled prefabs down

--- DragonLabs_IMD3901_TP/Assets/GeneralScripts/activateLobbyObjects.cs
+++ DragonLabs_IMD3901_TP/Assets/GeneralScripts/activateLobbyObjects.cs
@@ -33,13 +33,19 @@ public class activateLobbyObjects : MonoBehaviour
             {
                 int randomNum = Random.Range(0, 4);//choose a random player from 0 to 3 in the array
 
-                Debug.Log("random Num: " + randomNum);
+                Debug.Log("random Num PC local: " + randomNum);
                 SinglePCPlayerList[randomNum].SetActive(true);//turn the player on
                 //SinglePCPlayer.SetActive(true);//turn on PC player prefab
             }
             else if(staticClass.VROn)//if VR mode was chosen
             {
-                SingleVRPlayer.SetActive(true);//turn on VR player prefab
+
+                int randomNum = Random.Range(0, 4);//choose a random player from 0 to 3 in the array
+
+                Debug.Log("random Num VR local: " + randomNum);
+                SingleVRPlayerList[randomNum].SetActive(true);//turn the player on
+
+                //SingleVRPlayer.SetActive(true);//turn on VR player prefab
 
             }
         }
---
+++ DragonLabs_IMD3901_TP/Assets/GeneralScripts/sceneManagement/localPlayerLobbySpawn.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class localPlayerLobbySpawn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject localPlayer;
+    [SerializeField] Vector3 lobbyStartPos;
+
+
+    void Start()
+    {
+        localPlayer = GameObject.FindWithTag("Player");//find the player
+
+        Debug.Log("local player: " + localPlayer);
+
+        lobbyStartPos = new Vector3(0.05f, 0.46f, -19.02f);//set the spawn position
+
+        localPlayer.transform.position = lobbyStartPos;//make the player spawn here
+        Debug.Log("local player transform: " + localPlayer.transform.position );
+
+    }
+
+
+}
